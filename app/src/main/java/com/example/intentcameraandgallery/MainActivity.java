7ef7b04001ad1e543857bfe0bdd2e3c8d851cd52
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-05-25 21:08:38 +0700

    open camemra

--- app/src/main/java/com/example/intentcameraandgallery/MainActivity.java
+++ app/src/main/java/com/example/intentcameraandgallery/MainActivity.java
@@ -1,24 +1,33 @@
 package com.example.intentcameraandgallery;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-
-
 import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.Settings;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
 public class MainActivity extends AppCompatActivity {
 
-    Button mBtnCamera,mBtnGallery;
+    Button mBtnCamera, mBtnGallery;
     ImageView mImg;
     int REQUEST_CODE_CAMERA = 123;
+    ActivityResultLauncher<Intent> mActivityResultLauncher;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +35,16 @@ public class MainActivity extends AppCompatActivity {
         mBtnCamera = findViewById(R.id.buttonCamera);
         mBtnGallery = findViewById(R.id.buttonGallery);
         mImg = findViewById(R.id.imageView);
+
+        mActivityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        Log.d("BBB","Đã nhảy vào");
+                    }
+                }
+        );
 
         mBtnCamera.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,14 +56,29 @@ public class MainActivity extends AppCompatActivity {
 //                intent.setData(uri);
 //                startActivity(intent);
                 // Kiểm tra quyền camera đã được Granted
-                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
                     ActivityCompat.requestPermissions(
                             MainActivity.this,
                             new String[]{Manifest.permission.CAMERA},
                             REQUEST_CODE_CAMERA
                     );
+                }else{
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    mActivityResultLauncher.launch(intent);
                 }
             }
         });
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CODE_CAMERA) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                mActivityResultLauncher.launch(intent);
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
 }
